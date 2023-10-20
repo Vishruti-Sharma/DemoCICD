4fd86f214d9d22da0f72e99fed6d47b74f6bb1b6
vishruti-dev <66864698+vishruti-dev@users.noreply.github.com>
2023-10-20 13:30:21 +0530

    first

---
+++ src/main/java/com/example/DemoCicdApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCicdApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(DemoCicdApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
